7eadc1d78d8bea756efcc14e40c56c78b750da04
Serhii Babinets <sbabinets@outlook.com>
2023-08-21 12:42:38 +0300

    v2

--- src/Dotenv.php
+++ src/Dotenv.php
@@ -7,50 +7,30 @@ namespace Env;
 use \ErrorException;
 use \ValueError;
 
-use function \realpath;
 use function \parse_ini_file;
+use function \realpath;
 
+use const \INI_SCANNER_RAW;
 use const \INI_SCANNER_TYPED;
 
 final class Dotenv
 {
-    /**
-     * @param string $path      path to .env file.
-     * @param bool $overwrite   overwrite existing `$_ENV` keys?
-     */
-    public function __construct(
-        string $path,
-        bool $overwrite = true
-    ) {
-        $env = static::load($path);
-        $_ENV = ($overwrite)
-            ? $env + $_ENV
-            : $_ENV + $env;
-    }
-
-    /**
-     * Parse and retrieve content from .env file.
-     *
-     * @param string $path      path to .env file.
-     * @return array            associative array of .env keys and values.
-     * @throws ValueError       .env was not found at provided path.
-     * @throws ErrorException   corrupted .env, unable to parse.
-     * @see https://www.php.net/manual/en/function.parse-ini-file.php
-     */
-    public static function load(string $path): array
+    public static function toArray(string $path, bool $strict = true): array
     {
-        $path = realpath($path);
-        if ($path === false) {
-            throw new ValueError("File $path not found");
+        $realpath = realpath($path);
+        if (false === $realpath) {
+            throw new ValueError("File `$path` not found");
         }
         $env = parse_ini_file(
-            filename: $path,
+            filename: $realpath,
             process_sections: false,
-            scanner_mode: INI_SCANNER_TYPED
+            scanner_mode: ($strict)
+                ? INI_SCANNER_RAW
+                : INI_SCANNER_TYPED
         );
-        if ($env === false) {
-            throw new ErrorException("Unable to parse $path");
+        if ($env) {
+            return $env;
         }
-        return $env;
+        throw new ErrorException("Unable to parse $realpath");
     }
 }
